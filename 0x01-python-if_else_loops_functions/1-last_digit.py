c56951a3dcc3ef587c1233b71eecb56b64056e0d
hollaberry <you@example.com>
2021-11-17 08:56:55 -0800

    error out

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ else:
      rem = number % 10
 if rem > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, rem))
-elif rem  == 0:
+elif rem == 0:
     print("Last digit of {} is {} and is 0".format(number, rem))
 elif rem < 6 and rem != 0:
     print("Last digit of {} is {} and is less\
