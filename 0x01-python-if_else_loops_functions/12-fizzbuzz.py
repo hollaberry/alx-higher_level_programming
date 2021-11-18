f66515ebb4072954f3f4f193af8efd71f63d32b3
hollaberry <you@example.com>
2021-11-18 07:14:07 -0800

    error corrected

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 != 0:
-            print("Fizz")
+            print("Fizz", end=" ")
         elif i % 5 == 0 and i % 3 != 0:
-            print("Buzz")
+            print("Buzz", end=" ")
         elif i % 5 == 0 and i % 3 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", end=" ")
         else:
             print(i, end=" ")
