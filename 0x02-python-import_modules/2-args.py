1485e7784a074cb28080e5a5ba9501dd3ddeba58
hollaberry <you@example.com>
2021-11-19 02:23:37 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    p = len(sys.argv)
-    if p == 1:
-        print("{} arguments.".format(p - 1))
-    elif p == 2:
-        print("{} argument:".format(p - 1))
+    num = len(sys.argv)
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
     else:
-        print("{} arguments:".format(p - 1))
-    for i in range(1, p):
-        print("{}: {}".format(p, sys.argv[i]))
+        print("{} arguments:".format(num - 1))
+
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
