676b107dd7f272ac2e75139ccf810ba70e2789c3
hollaberry <you@example.com>
2021-11-22 09:51:41 -0800

    using copy method

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
     num = len(my_list)
     num = num - 1
-    new_list = my_list[:]
+    new_list = my_list.copy()
     if idx < 0 or idx > num:
-        return my_list[:]
+        return my_list.copy()
     else:
         new_list[idx] = element
     return new_list
