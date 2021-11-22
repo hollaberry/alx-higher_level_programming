259f5fce1855009f3b98a0b0d95d91eb842f5373
hollaberry <you@example.com>
2021-11-22 08:22:21 -0800

    ërror out

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     num = len(my_list)
-    if idx < 1 or idx > num:
+    num = num - 1
+    if idx < 0 or idx > num:
         return None
 print(my_list[idx])
