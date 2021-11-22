90922e9748cf619c50ffee5e9c097d0fe3440408
hollaberry <you@example.com>
2021-11-22 09:25:03 -0800

    error out

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,9 +4,8 @@ def new_in_list(my_list, idx, element):
     num = num - 1
     new_list = my_list
     if idx < 0 or idx > num:
-        return new_list
+        return my_list
     else:
         new_list[idx] = element
     return new_list
-    return my_list
     
