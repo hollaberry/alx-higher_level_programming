0278b01986fa01e62fb31e17141067ad469dbc81
hollaberry <you@example.com>
2021-11-22 12:12:56 -0800

    oust char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    final_username = my_string.translate({ ord(c): None for c in 'cC'})
-    return final_username
+    string = my_string.translate({ord(i): None for i in 'cC'})
+    return string
