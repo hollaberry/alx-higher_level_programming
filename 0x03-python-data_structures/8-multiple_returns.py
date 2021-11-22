cad038a01fbdebf167b7801a31f3d5fa3b410eb1
hollaberry <you@example.com>
2021-11-22 13:42:28 -0800

    error out

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     if num < 1:
         return None
     str = ''.join(sentence)
-    num = len(str)
+    num = len(str) - 1
     return num
-    return str[1]
+    return str[0]
