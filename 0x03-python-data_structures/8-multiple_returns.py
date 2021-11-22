0ad452755b70848052a7de8853d294fb6489a038
hollaberry <you@example.com>
2021-11-22 13:51:09 -0800

    tuple  power

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    str = ''.join(sentence)
-    num = len(str)
-    if num < 1:
-        return None
+    tup = ()
+    if len(sentence) == 0:
+        tup = 0, "None"
     else:
-        return str[0]
-    num = len(str) - 1
-    return num
+        tup = len(sentence), sentence[0]
+        return tup
