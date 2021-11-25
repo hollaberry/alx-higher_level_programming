4b1ac0f7b5e933612f1ae11790a846cefec10c12
hollaberry <you@example.com>
2021-11-25 13:52:50 -0800

    error out

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = sorted(my_list)
+    new_list = set(my_list)
     total = 0
     for i in range(0, len(new_list)):
         if new_list[i] != new_list[i - 1]:
