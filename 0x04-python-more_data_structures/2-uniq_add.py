daef0ebe7554de165e9c5f2e226b69343cefebd0
hollaberry <you@example.com>
2021-11-25 13:55:20 -0800

    o

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = set(my_list)
+    new_list = sorted(my_list)
     total = 0
-    for i in range(0, len(new_list)):
+    for i in new_list:
         if new_list[i] != new_list[i - 1]:
             total += new_list[i]
     return total
