a6d0245bb6e9033edcc9e7f8fabdf831bbf2d3f6
hollaberry <you@example.com>
2021-11-25 13:45:43 -0800

    task 2 - 14

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = sorted(my_list)
+    total = 0
+    for i in range(0, len(new_list)):
+        if new_list[i] != new_list[i - 1]:
+            total += new_list[i]
+    return total
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    container = list(a_dictionary)
+    count = len(container)
+    return count
