2e5c7c78e15c44deb40533aaa99d7ee5806b8044
hollaberry <you@example.com>
2021-11-26 02:05:40 -0800

    error out

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     high_key = list(a_dictionary.keys())[0]
-    max = a_dictionary[start_key]
+    max = a_dictionary[high_key]
     for k, v in a_dictionary.items():
         if v > max:
             high_key = k
