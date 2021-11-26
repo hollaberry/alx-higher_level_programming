c125f0010df21c89732188e5016fdf352fd7eabc
hollaberry <you@example.com>
2021-11-26 02:02:29 -0800

    best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    high_key = list(a_dictionary.keys())[0]
+    max = a_dictionary[start_key]
+    for k, v in a_dictionary.items():
+        if v > max:
+            high_key = k
+            max = v
+    return high_key
