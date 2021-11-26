b265bf9837309eafd6f3bb4efc84624e2744b211
hollaberry <you@example.com>
2021-11-26 03:53:08 -0800

    whitespace error out

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dict = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string and type(roman_string) is str:
         num = 0
         for i in range(len(roman_string)):
