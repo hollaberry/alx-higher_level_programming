0e0c0d2cfef2c5525e8225c716e1eeed2213d66a
hollaberry <you@example.com>
2021-12-12 12:30:40 -0800

    exception 0-6

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        j = 0
+        for i in my_list:
+            if j < x:
+                print("{}".format(i), end="")
+                j = j + 1
+        print('')
+        return j
+    except SyntaxError:
+        print("Error Syntax")
+        
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    my_list_3 = []
+
+    for i in range(list_length):
+        try:
+            x = my_list_1[i] / my_list_2[i]
+        
+        
+        except ZeroDivisionError:
+        print("division by 0")
+        x = 0
+        except TypeError:
+            print("wrong type")
+            x = 0
+        except IndexError:
+            print(out of range)
+            x = 0
+        finally:
+            my_list_3.append(x)
+    return my_list_3
+        
