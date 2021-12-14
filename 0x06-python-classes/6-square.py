93a6bf1954c9c8290ef339f806cae4a8e8d4aa8d
hollaberry <you@example.com>
2021-12-14 10:04:02 -0800

    error out

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,17 +10,12 @@ class Square:
         self.size = size
         self.position = position
 
-    """Getter"""
+    """Size getter"""
     @property
     def size(self):
         return (self.__size)
 
-    """position getter"""
-    @property
-    def position(self):
-        return (self.__position)
-
-    """Setter"""
+    """Size setter"""
     @size.setter
     def size(self, value):
         if type(value) != int:
@@ -29,22 +24,28 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    """Position getter"""
+    @property
+    def position(self):
+        return (self.__position)
+
     """Position setter"""
     @position.setter
     def position(self, value):
         if (type(value) != tuple or len(value) != 2 or
-            type(value[0]) != int or type(value[1]) != int or
-            value[0] < 0 or value[1] < 0):
+        type(value[0]) != int or type(value[1]) != int
+        or value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-    """Create public instance method: def area(self)"""
+    """Create public instance method: area"""
     def area(self):
         sq_area = self.__size ** 2
         return (sq_area)
 
     """Create public instance method: my_print
-    prints square with # character"""
+    Line is not filles with spaces when position[1] > 0
+    """
     def my_print(self):
         if self.__size > 0:
             for x in range(self.position[1]):
