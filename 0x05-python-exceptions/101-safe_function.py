addc652ab4fe6077b0edbc6fad918f5a0b0317db
hollaberry <you@example.com>
2021-12-14 04:44:11 -0800

    funct safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
+    import sys
+    try:
+        dum = fct(*args)
+        return dum
+    except Exception as en:
+        print("Ëxception: {:s}".format(str(en)), file=sys.stderr)
+        return None
