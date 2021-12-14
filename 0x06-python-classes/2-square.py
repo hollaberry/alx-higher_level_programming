581be871554447cd4bc520f1df77245d392d4dea
hollaberry <you@example.com>
2021-12-14 07:04:39 -0800

    square with exception

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""Write a class square"""
+
+
 class Square:
+    """Represent a square"""
+
+
     def __init__(self, size=0):
+        """initialise a private instance attribute to define a square
+        Raise the type or value error exception according to given condition"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
-
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
