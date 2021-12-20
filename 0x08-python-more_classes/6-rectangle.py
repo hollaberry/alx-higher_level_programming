be8aa901d9ba85b7e9e42c7d641c9c31a1dd08c4
hollaberry <you@example.com>
2021-12-20 13:08:22 -0800

    remove whitespaces

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -14,7 +14,6 @@ class Rectangle:
 
     number_of_instances = 0
 
-    
     def __init__(self, width=0, height=0):
         """initialise the attribute width and height"""
         self.width = width
