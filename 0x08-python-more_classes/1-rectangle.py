4b8d335e4553065defe9f4a38f4f975b3797650a
hollaberry <you@example.com>
2021-12-22 01:45:55 -0800

    erroor out

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,12 +13,14 @@ class Rectangle:
            width: width of the rectangle
            height: height of the rectangle
        """
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
         """Retrieves the width of a Rectangle instance"""
+
         return self.__width
 
     @width.setter
@@ -38,6 +40,7 @@ class Rectangle:
     @property
     def height(self):
         """Retrieves the heigt of a rectangle instance"""
+
         return self.__height
 
     @height.setter
