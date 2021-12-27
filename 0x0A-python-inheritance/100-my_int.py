51e65d2b2f30aab62aa58070e9d271a6e4db7f13
hollaberry <you@example.com>
2021-12-27 08:32:28 -0800

    advance task

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to create class MTINT"""
+
+class MyInt(int):
+    """Class inheriting from int,
+    But reverses the behaviou of != and ==.
+    """
+
+    def __eq__(self, other):
+        """Opposing the equality."""
+
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Ïnequality becomes equality."""
+
+        return super().__eq__(other)
+          
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module to add new attribute"""
+
+
+def add_attribute(an_obj, an_attr, a_value):
+    """Check if an_attr of value a_value can be added to an_obj.
+     
+    Args:
+        - an_obj: object to add the attribute to
+        - an_attr: name of the attribute
+        - a_value: value of the attribute to add
+    """
+
+    if hasattr(an_obj, '__slots__') and not hasattr(an_obj, an_attr):
+        raise TypeError("can't add new attribute")
+    if not hasattr(an_obj, '__slots__') and not hasattr(an_obj, an_attr):
+        raise TypeError("can't add new attribute")
+
+    setattr(an_obj, an_attr, a_value)
