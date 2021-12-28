a9febed28af97f0856ac615b1e6082d56d0806b5
hollaberry <you@example.com>
2021-12-28 04:44:05 -0800

    edit task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,8 +2,6 @@
 """Module to check for an instance"""
 
 def is_same_class(obj, a_class):
-    """function to check fo ran instance"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    """function to check if obj is an instance of a_class"""
+
+    return True if type(obj) is a_class else False
