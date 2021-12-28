60d7f5f51a7ad67df564126b5039593c71ea6fc8
hollaberry <you@example.com>
2021-12-28 05:42:17 -0800

    err out

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for inheriting a class from another class"""
 
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
@@ -9,7 +10,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Initialise an instance.
         with width and height as private"""
-        
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
