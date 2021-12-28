4b33d6607e3dfe1ffb0ffc4896f830d81e248703
hollaberry <you@example.com>
2021-12-28 05:13:34 -0800

    edit task

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module 4-inherits_from
+finds if the object is an instance of a
+class tha inherited (directly or indirectly) from the specified class."""
+
+
 def inherits_from(obj, a_class):
     """Function to check the kind of an object"""
 
