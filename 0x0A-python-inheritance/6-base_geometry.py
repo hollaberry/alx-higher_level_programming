c0807ab6e2e92cdd386d4869623f5c38dba9ebf0
hollaberry <you@example.com>
2021-12-28 05:25:43 -0800

    err out

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Module for a class BaseGeometry"""
 
+
 class BaseGeometry:
     """Class with public instance method"""
     def area(self):
         """Function to rasie an exception"""
+
         raise Exception("area() is not implemented")
