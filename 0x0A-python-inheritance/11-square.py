d27022ab2c170abccd8f4abe3469040764ad3d69
hollaberry <you@example.com>
2021-12-28 05:49:34 -0800

    err out

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,6 +5,7 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Class Square inheriting from Rectangle"""
 
@@ -18,7 +19,6 @@ class Square(Rectangle):
 
     def __str__(self):
         return str("[Square] {}/{}".format(self.__size, self.__size))
-
 
     def area(self):
         """Area method implementation"""
