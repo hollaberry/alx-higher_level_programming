cac117e34607c8a5616ec8fca7ab72f665955c74
hollaberry <you@example.com>
2021-12-28 04:57:58 -0800

    task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module 3-is_kind_of_class"""
+
+
 def is_kind_of_class(obj, a_class):
     """Function to check the kind of an object"""
+
     return isinstance(obj, a_class)
