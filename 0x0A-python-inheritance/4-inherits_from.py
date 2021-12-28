1c145e2e45f888868fdc13569cef6be23f7e1893
hollaberry <you@example.com>
2021-12-28 05:03:57 -0800

    task 4 edit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
     """Function to check the kind of an object"""
-    return issubclass(obj, a_class)
+
+    if type(obj) == a_class:
+        return False
+    else:
+        return isinstance(obj, a_class)
