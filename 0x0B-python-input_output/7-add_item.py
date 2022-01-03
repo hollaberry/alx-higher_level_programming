30235622fd44c8e6601b5ebf19a89b3476c5cd32
hollaberry <you@example.com>
2022-01-03 02:05:39 -0800

    err o

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Module to add all argument"""
+"""This module creates a script that adds args to Python list and saves them"""
 
-from sys import argv
 from os import path
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-file = "add_item.json"
-json_list = []
-
-if path.exists(file) is False:
+if path.exists("add_item.json") is False:
     save_to_json_file([], "add_item.json")
-json_list = load_from_json_file("add_item.json")
+my_list = load_from_json_file("add_item.json")
 for i in range(1, len(argv)):
-    json_list.append(argv[i])
-save_to_json_file(json_list, file)
-    
+    my_list.append(argv[i])
+save_to_json_file(my_list, "add_item.json")
