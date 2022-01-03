a8bf5103b40251b1cb109ba49de032aa80d91a98
hollaberry <you@example.com>
2022-01-03 02:02:32 -0800

    add_item task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """Module to add all argument"""
-import json
-import sys
-import os.path
+
+from sys import argv
+from os import path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 file = "add_item.json"
 json_list = []
 
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
-
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
-
+if path.exists(file) is False:
+    save_to_json_file([], "add_item.json")
+json_list = load_from_json_file("add_item.json")
+for i in range(1, len(argv)):
+    json_list.append(argv[i])
 save_to_json_file(json_list, file)
     
