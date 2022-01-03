a91050e113e773930046b77c9b3d105057eea390
hollaberry <you@example.com>
2022-01-03 02:36:17 -0800

    er out

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,9 +5,9 @@
 class student:
     """student"""
     def __init__(self, first_name, last_name, age):
-        self.age = age
         self.first_name = first_name
         self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """to_json"""
