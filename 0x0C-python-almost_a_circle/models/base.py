05b26e974ae7036153a7a50faed6244b90a3c537
hollaberry <you@example.com>
2022-02-24 05:12:20 -0800

    adding first commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Implementation of a base class for allother classes in this project. The goal
+of it is to manage id attribute in all future classes to avoid duplicating
+the same code (by extension, same bugs)
+"""
+
+class Base:
+    """
+     implementation 
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ class constructor """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
