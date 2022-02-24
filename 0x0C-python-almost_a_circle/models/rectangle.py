5b3b35437670b60e509d56ae2bc56934d35a9129
hollaberry <you@example.com>
2022-02-24 06:15:23 -0800

    update rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,15 +13,86 @@ class Rectangle(Base):
         """
         initialisation
         """
-        super().__init__(id)
+        
 
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
+    @property
+    def width(self):
+        """Retrieves the width attribute."""
 
-    def __str__(self) -> str:
-        """string representation
+        return self.__width
+
+    @property
+    def height(self):
+        """Retrieves the height attribute."""
+
+        return self.__height
+
+    @property
+    def x(self):
+        """Retrieves the x attribute."""
+
+        return self.__x
+
+    @property
+    def y(self):
+        """Retrieves the y attribute."""
+
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """sets the width attribute."""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets the height attribute."""
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Sets the x attribute. """
+
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """Sets the y attribute. """
+
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Calculator the area of a Rectangle instance
+       
+        Returns: area
         """
-        return "[Rectangle] ({}) {}/{} - {}/{}" \
-            .format(self.id, self.x, self.y, self.width, self.height)
+
+        return self.__width * self.__height
+
+    def display(self):
+        """Prints the rectangle instance with the # character"""
+
+        for y in range(
