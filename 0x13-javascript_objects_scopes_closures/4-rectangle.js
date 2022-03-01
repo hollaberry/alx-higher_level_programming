9a51df4d834fdc2da33fe547b40b1cb8b25e83c0
olatunde <lawalolatunde@ymail.com>
2022-03-01 15:48:19 +0000

    rotdou

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate () {
+      const a = this.width;
+      this.width = this.height;
+      this.height = a;
+  }
+  double () {
+      this.width = this.width * 2;
+      this.height = this.height * 2;
+ }
+}
+module.exports = Rectangle;
