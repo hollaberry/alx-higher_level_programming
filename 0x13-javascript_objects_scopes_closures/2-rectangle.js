66d6ec213c9756b2f6552e86cda46fa96a75331e
olatunde <lawalolatunde@ymail.com>
2022-03-01 14:55:49 +0000

    error out

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,9 @@
 class Rectangle {
   constructor (w, h) {
       if (((w = parseInt(w) > 0) && ((h = parseInt(h)) > 0)) {
-      this.width = w;
-      this.height = h;
-}
-}
+        this.width = w;
+        this.height = h;
+      }
+   }
 }
 module.exports = Rectangle;
