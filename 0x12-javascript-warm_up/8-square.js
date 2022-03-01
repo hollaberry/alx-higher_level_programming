ce7176d9f3b7ca949b9594a3ce1f5284c8ade8bb
olatunde <lawalolatunde@ymail.com>
2022-03-01 10:33:16 +0000

    erroout

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-    console.log('Missing size');
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
 } else {
-const m = number(process.argv[2]);
-for (let i = 0; i < m; i++) {
-    console.log('X'.repeat(m))
-} 
-}
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
