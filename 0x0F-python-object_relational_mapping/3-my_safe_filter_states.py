7245fb34c44bb0f8fe1ced72622c167952b17dee
olatunde <lawalolatunde@ymail.com>
2022-03-07 09:38:27 +0000

    New tasks

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""List all states from a given db sorted in ascending order by id, Username, password, and database names are given as user args
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+   db = MySQLdb.connect(host='localhost',
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3])
+cur = db.cursor()
+newcmd = """SELECT id, name
+            FROM states 
+            WHERE name=%s 
+            ORDER BY id ASC;"""
+cur.execute(newcmd, (sys.arg[4],))
+stateswithN = cur.fetchall()
+
+for state in stateswithN:
+    print(state)
+
+cur.close()
+db.close()
---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""List all states from a given db sorted in ascending order by id, Username, password, and database names are given as user args
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+   db = MySQLdb.connect(host='localhost',
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3])
+cur = db.cursor()
+newcmd = """SELECT cities.id, cities.name, states.name
+            FROM states
+            INNER JOIN cities ON states.id = cities.state_id  
+            ORDER BY cities.id ASC;""".
+cur.execute(newcmd)
+allcities = cur.fetchall()
+for city in allcities:
+    print(state)
+
+cur.close()
+db.close()
