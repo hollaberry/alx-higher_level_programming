0514f00438fb920c393a2cfc4c5c70b6f4bb719c
olatunde <lawalolatunde@ymail.com>
2022-03-07 10:13:24 +0000

    edit task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """List all states from a given db sorted in ascending order by id, Username, password, and database names are given as user args
 """
-import sys
-import MySQLdb
-
 if __name__ == "__main__":
+
+   import MySQLdb
+   import sys
+
    db = MySQLdb.connect(host='localhost',
                         port=3306,
                         user=sys.argv[1],
                         passwd=sys.argv[2],
                         db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT id, name FROM states ORDER BY id ASC")
-allstates = cur.fetchall()
+   
+   cur = db.cursor()
+   cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+   all_states = cur.fetchall()
 
-for state in allstates:
-    print(state)
-
-cur.close()
-db.close()
-                        
+   for state in all_states:
+      print(state)
