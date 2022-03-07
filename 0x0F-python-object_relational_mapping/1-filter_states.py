90721d9ebfa7c1a3c2ce28c1c338802aab5e5521
olatunde <lawalolatunde@ymail.com>
2022-03-07 11:00:24 +0000

    pycodestyle error removed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""List all states from a given db sorted in ascending order by id, Username, password, and database names are given as user args
+"""List all states from a given db sorted in
+ascending order by id, Username, password,
+and database names are given as user args
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost',
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
     newcmd = """SELECT id, name
-                FROM states 
-                WHERE name LIKE BINARY 'N%' 
+                FROM states
+                WHERE name LIKE BINARY 'N%'
                 ORDER BY id ASC;"""
     cur.execute(newcmd)
     stateswithN = cur.fetchall()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""List all states from a given db sorted in ascending order by id, Username, password, and database names are given as user args
+"""List all states from a given db sorted in
+ascending order by id, Username, password,
+and database names are given as user args
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost',
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
     newcmd = """SELECT id, name
-                FROM states 
-                WHERE name LIKE BINARY '{}' 
+                FROM states
+                WHERE name LIKE BINARY '{}'
                 ORDER BY id ASC;""".format(sys.argv[4])
     cur.execute(newcmd)
     stateswithN = cur.fetchall()
 
     for state in stateswithN:
-       print(state)
+        print(state)
 
     cur.close()
     db.close()
