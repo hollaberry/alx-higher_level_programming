040b0933ebc3e0baf9805b561d8e494f2e9c52e1
olatunde <lawalolatunde@ymail.com>
2022-03-08 13:15:29 +0000

    edit session

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,12 +7,13 @@ import sys
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from model_state import Base, State
-from sqlalchemy.sql.expression import update
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     result = session.query(State).filter(State.id == 2)
     result.update({"name": ("New Mexico")})
 
