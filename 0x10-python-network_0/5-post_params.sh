9f803e237c5b7edd9bd3d13816e3141e40f16169
olatunde <lawalolatunde@ymail.com>
2022-03-29 15:11:40 +0000

    adding subject

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # takes in a url, sends a POST request to the passed URL
-# displays the body of the response
 curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
